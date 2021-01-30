3cc03052e6c8ac149875debe26a3725295aeaea4
EC2 Default User <ec2-user@ip-172-31-78-222.ec2.internal>
2021-01-30 20:06:53 +0000

    Añadido comentario de prueba

--- todos/list.py
+++ todos/list.py
@@ -5,7 +5,7 @@ from todos import decimalencoder
 import boto3
 dynamodb = boto3.resource('dynamodb')
 
-
+# Funcion para listar notas
 def list(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
